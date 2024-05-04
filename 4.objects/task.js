d85490d6753032d4313e23bdf8b67b35f289cfff
peshkova <PeshkovaKE@yandex.ru>
2024-05-04 12:08:17 +0700

    Четвертое задание полностью готово

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,53 @@
 function Student(name, gender, age) {
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
+    this.marks = [];
+
   
 }
 
 Student.prototype.setSubject = function (subjectName) {
+    this.subject = subjectName;
   
 }
 
-Student.prototype.addMarks = function (...marks) {
-  
+Student.prototype.addMarks = function (...marksToAdd) {
+        
+    if (this.hasOwnProperty('marks') == true)
+    {
+       
+        return this.marks.push(...marksToAdd);
+    }
+    
+    
+    
 }
 
 Student.prototype.getAverage = function () {
-  
+    
+    if (this.hasOwnProperty('marks') == false){
+        return 0;
+    }
+    if (this.marks.length == 0 ){
+        return 0;
+    }
+    else {
+    
+        let sum = 0;
+        for (let i = 0; i < this.marks.length; i += 1) {
+        sum += this.marks[i];
+        } 
+        return sum / this.marks.length;
+    }
+   
 }
 
 Student.prototype.exclude = function (reason) {
-  
+    delete this.marks;
+    delete this.subject;
+    this.excluded = reason;
+
 }
+
+const student1 = new Student("Nikolay", "male", 21);
